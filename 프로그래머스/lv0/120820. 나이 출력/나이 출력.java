32657fc3f77e6cd878fabd4abbfc3058b13329c8
BOB <56067193+adminhelper@users.noreply.github.com>
2023-09-24 23:43:45 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;    
+        int standard = 2022;
+        
+        answer = standard - age + 1;
+    
+        
+        return answer;
+    }
+}
